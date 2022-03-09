d5b75d8292a5b0e253132181b102b8f7b20c0310
elzelalo <83767002+elzelalo@users.noreply.github.com>
2022-03-09 17:06:54 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const valid = () => {
+
+    var elt = document.querySelector('textarea');
+	    console.log(elt.value);
+        var elt = document.querySelector('textarea');
+		    
+    function longueur(){
+    
+        var texte = document.getElementById('textea').value;
+        console.log(`${texte.length}`);
+    }
+    
+}
+
+
+
+        
